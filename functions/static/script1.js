f917fcf2f1a3dc7efcbba6fc9e2ba317c0abbcb7
Ritvik Rana <ranaritvik2002@gmail.com>
2025-01-11 23:44:16 +0530

    Initial Commit

---
+++ functions/static/script1.js
@@ -0,0 +1,147 @@
+//Typing animation script
+var typed = new Typed(".typing",{
+    strings: ["Friend","Teacher","Helper","Go-to bot"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true,
+});
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+let val;
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+        scrollBtn.style.display = "block";
+    }else{
+        nav.classList.remove("sticky");
+        scrollBtn.style.display = "none";
+    }
+}
+
+// Side Navigation Menu Js
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function(){
+    navBar.classList.add("active");
+    menuBtn.style.opacity ="0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflowX = "hidden";
+    scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+    navBar.classList.remove("active");
+    menuBtn.style.opacity ="1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflowX = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+}
+// Side Navifation Bar close while we click on navigation links
+
+let navLinks = document.querySelectorAll(".menu li a");
+for(var i = 0; i< navLinks.length; i++){
+    navLinks[i].addEventListener("click", function(){
+        navBar.classList.remove("active");
+        menuBtn.style.opacity = "1";
+        menuBtn.style.pointerEvents ="auto";
+    });
+}
+
+//ChatBox
+
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            openButton: document.querySelector('.button2'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        }
+
+        this.state = false;
+        this.messages = [];
+    }
+
+    display() {
+        const {openButton, chatBox, sendButton} = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox))
+
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({key}) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        // show or hides the box
+        if(this.state) {
+            chatbox.classList.add('chatbox--active')
+        } else {
+            chatbox.classList.remove('chatbox--active')
+        }
+    }
+
+    onSendButton(chatbox) {
+        var textField = chatbox.querySelector('input');
+        let text1 = textField.value
+        if (text1 === "") {
+            return;
+        }
+
+        let msg1 = { name: "User", message: text1 }
+        this.messages.push(msg1);
+
+        fetch('http://127.0.0.1:5000/predict', {
+            method: 'POST',
+            body: JSON.stringify({ message: text1 }),
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          })
+          .then(r => r.json())
+          .then(r => {
+            let msg2 = { name: "Sam", message: r.answer };
+            this.messages.push(msg2);
+            this.updateChatText(chatbox)
+            textField.value = ''
+
+        }).catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatbox)
+            textField.value = ''
+          });
+    }
+
+    updateChatText(chatbox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function(item, index) {
+            if (item.name === "Sam")
+            {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+            }
+            else
+            {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+            }
+          });
+
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML = html;
+    }
+}
+
+
+const chatbox = new Chatbox();
+chatbox.display();
