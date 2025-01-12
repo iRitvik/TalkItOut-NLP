63be805d10fcb38b326bfde23e70c7286e092e98
Ritvik Rana <ranaritvik2002@gmail.com>
2025-01-12 16:58:39 +0530

    Pre Deployment files added

---
+++ Pre Deployment Files/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+import sys
+import os
+
+# Add the parent directory to the system path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_get():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+ 
